37c8cd7911cc39255e6ced283044f9c06e69be2b
Hassan Khan <33081303+hassankhan98@users.noreply.github.com>
2025-09-21 10:19:53 -0400

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,16 +1,13 @@
-// firebase-config.js
-// IMPORTANT: Replace the firebaseConfig below with your project's config from Firebase Console > Project settings > SDK setup for web
-
+// Replace with your Firebase config
 const firebaseConfig = {
   apiKey: "AIzaSyBgG89RoBGc-Gqe2TYnO_iPSdtc8XxQwJw",
   authDomain: "pos-web-app-ed7e9.firebaseapp.com",
   projectId: "pos-web-app-ed7e9",
-  storageBucket: "pos-web-app-ed7e9.firebasestorage.app",
+  storageBucket: "pos-web-app-ed7e9.appspot.com",
   messagingSenderId: "386790885396",
   appId: "1:386790885396:web:83955d99b689cf377ebf01"
 };
 
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
-// Firestore reference
 const db = firebase.firestore();
